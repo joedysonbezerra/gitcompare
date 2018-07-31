810bf1129c1a1a3a58b1049246371bcaabb41646
Joédyson Bezerra <joedysonbezerra@gmail.com>
2018-07-30 23:37:27 -0300

    Conexão com a api feita

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,20 +2,45 @@ import React, {Component} from 'react';
 import logo from '../../assets/logo.png'
 import {Container, Form} from './style'
 import CompareList from '../../components/CompareList'
+import api from '../../services/api'
+import moment from 'moment';
+
 
 export default class Main extends Component{
   state={
-    	repositories:[]
+    repositoryInput:'',
+    repositories:[],
+  }
 
-      ],
+  handleAddRepository = async(e) =>{
+    e.preventDefault();
+
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput:'',
+        repositories:[...this.state.repositories, repository],
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
   }
 
   render() {
     return(
       <Container>
       <img src={ logo } alt="Github Compare"/>
-      <Form>
-        <input type="text" placeholder="Usuário/Repositório"/>
+      <Form onSubmit={this.handleAddRepository} >
+        <input
+        type="text"
+        placeholder="Usuário/Repositório"
+        value={this.state.repositoryInput}
+        onChange={e => this.setState({repositoryInput: e.target.value}) }
+        />
         <button type="submit">OK</button>
       </Form>
 
